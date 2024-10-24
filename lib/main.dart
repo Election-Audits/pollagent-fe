ba928fd622b0cf796b7b8feac341b5f6fd90f1d2
TheGadri <andygadri@gmail.com>
2024-10-24 23:29:20 +0000

    Project setup on dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pollagent_fe/src/core/logger/riverpod_log.dart';
+import 'package:pollagent_fe/src/core/service/router/router.dart';
+import 'package:pollagent_fe/src/core/theme/theme.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      observers: [RiverpodObserver()],
+      child: const MyApp(),
+    ),
+  );
+}
+
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MediaQuery.withClampedTextScaling(
+      maxScaleFactor: 1.5,
+      child: MaterialApp.router(
+        theme: context.themeData,
+        routerConfig: ref.read(goRouterProvider),
+      ),
+    );
+  }
+}
+
+
+
+/*
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -123,3 +157,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
